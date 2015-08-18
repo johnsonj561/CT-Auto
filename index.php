9a731f834335991de22e145d610b11fd713a487b
Justin Johnson <johnsonj561@gmail.com>
2015-08-17 23:45:38 -0400

    Content Updated

--- index.php
+++ index.php
@@ -1,164 +1,230 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
-    <title>Heroic Features - Start Bootstrap Template</title>
-
+    <link rel="icon" type="image/x-icon" href="img/wrench-gear-favicon-16-16.png" />
+    <title>C &amp; T Auto</title>
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Custom CSS -->
     <link href="css/heroic-features.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
+  </head>
+  <body>
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">C &amp; T Auto | Singer Island, FL</a>
         </div>
-        <!-- /.container -->
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li>
+              <a href="index.php">Home</a>
+            </li>
+            <li>
+              <a href="/view/contact.php">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container -->
     </nav>
 
     <!-- Page Content -->
-    <div class="container">
+    <div class="container" name="top">
 
-        <!-- Jumbotron Header -->
-        <header class="jumbotron hero-spacer">
-            <h1>A Warm Welcome!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
-            <p><a class="btn btn-primary btn-large">Call to action!</a>
+      <div class="col-lg-12">
+        <div class="col-lg-8">
+          <!-- Jumbotron Header -->
+          <header class="jumbotron black-border hero-spacer-left">
+            <div class="text-center">
+              <h1 class="jumbotron-title">Singer Island Auto Service</h1>
+              <!-- <h1><a href="tel:+15619097795" class="contact-link">561-629-7214</a></h1>  -->
+            </div>
+            <h2 class="text-center slogan">Providing automobile services to Singer Island, Riviera Beach, and West Palm Beach since
+              1992</h2> 
+            <p class="text-center">
+              <a href="tel:561-629-7214" class="btn btn-primary btn-large mobile-only call-now">
+                <span class="glyphicon glyphicon-phone-alt"></span>CLICK TO CALL NOW</a>
             </p>
-        </header>
-
-        <hr>
-
-        <!-- Title -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h3>Latest Features</h3>
-            </div>
+            <p class="text-center">
+              <a href="view/contact.php" class="btn btn-primary btn-large pc-only call-now">
+                <span class="glyphicon glyphicon-envelope"></span>CONTACT US NOW FOR SERVICE
+              </a>
+            </p>
+          </header>
         </div>
-        <!-- /.row -->
-
-        <!-- Page Features -->
-        <div class="row text-center">
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
-                    <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
-                    <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
-                    <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-3 col-sm-6 hero-feature">
-                <div class="thumbnail">
-                    <img src="http://placehold.it/800x500" alt="">
-                    <div class="caption">
-                        <h3>Feature Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        <p>
-                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
+        <div class="col-lg-4 hero-spacer">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14247.030111687036!2d-80.04179645375056!3d26.783997999909158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d8d4f234f64211%3A0x4292d655348e98bb!2s1260+Plaza+Cir%2C+West+Palm+Beach%2C+FL+33404!5e0!3m2!1sen!2sus!4v1439737715695" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+          <div class="text-center image-description">Located on the South East corner of Singer Island. Take 
+            Blue Heron Blvd East over the bridge to Singer Island. Find us on your right hand side, across 
+            from Grator Gator Food Market.
+          </div>
         </div>
-        <!-- /.row -->
+      </div>
 
-        <hr>
+      <!-- Title -->
+      <div class="row">
+        <div class="col-lg-12 title-row feature-title">
+          <h1>Automobile Services Available</h1>
+        </div>
+      </div>
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
+      <!-- Page Features - ROW 1 -->
+      <div class="row text-center" name="about" id="about">
+        <div class="col-md-3 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/engine-repairs-500x300.jpg" alt="Engine diagnostic and repair">
+            <div class="caption">
+              <h2 class="thumbnail-title">Engine Repairs</h2>
+              <p>Our experienced staff is familiar with all major vehicle engines.</p>
+              <!--<p>
+<a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">
+More Info</a>
+</p> -->
             </div>
-        </footer>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/brake-service-500x300.jpg" alt="Brake and rotor service">
+            <div class="caption">
+              <h2 class="thumbnail-title">Brakes &amp; Rotors</h2>
+              <p>Get new brake pads or rotors installed for an affordable price.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/engine-warning-lights-500x300.jpg" alt="Engine warning lights">
+            <div class="caption">
+              <h2 class="thumbnail-title">Vehicle Diagnostics</h2>
+              <p>Have your vehicle diagnosed to ensure it is running efficiently.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/new-used-tires-500x300.jpg" alt="New and used tires for sale">
+            <div class="caption">
+              <h2 class="thumbnail-title">New &amp; Used Tires</h2>
+              <p>Get your worn tires replaced or have a flat repaired.</p>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <!-- Page Features - ROW 2 -->
+      <div class="row text-center" name="about" id="about">
+        <div class="col-md-3 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/air-conditioner-500x30.jpg" alt="Air conditioning and coolant">
+            <div class="caption">
+              <h2 class="thumbnail-title">Cooling Systems</h2>
+              <p>It's Florida, make sure your air conditioner is functioning properly!</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/interstate-battery-service-500x300.jpg" alt="Car battery install and charge">
+            <div class="caption">
+              <h2 class="thumbnail-title">Car Batteries</h2>
+              <p>Providing our customers with the most trusted batteries available.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/oil-change-500x300.jpg" alt="Get your oil and filter changed">
+            <div class="caption">
+              <h2 class="thumbnail-title">Oil Changes</h2>
+              <p>Protect your engine by changing your oil and filter every 3K miles.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 hero-feature">
+          <div class="thumbnail">
+            <img src="img/engine-diagnostics-500x300.jpg" alt="Vehicle tune up services">
+            <div class="caption">
+              <h2 class="thumbnail-title">Vehicle Tune Ups</h2>
+              <p>Have your vehicle tested and tuned to provide maximum performance.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row"><a href="#top">Back to Top</a></div>
     </div>
-    <!-- /.container -->
+    
+    <!-- Social Media Buttons -->
+    <row class="text-center social-media">
+      <ul class="share-buttons">
+        <li><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.kbsoftwaresolutions.com&t=" 
+               title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' 
+          + encodeURIComponent(document.URL) + '&t=' + encodeURIComponent(document.URL)); return false;">
+          <img src="img/social-media/Facebook.png" alt="facebook-logo"></a></li>
+        <li><a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fwww.kbsoftwaresolutions.com&text=:%20http%3A%2F%2Fwww.kbsoftwaresolutions.com" 
+               target="_blank" 
+               title="Tweet" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) 
+          + ':%20'  + encodeURIComponent(document.URL)); return false;">
+          <img src="img/social-media/Twitter.png" alt="twitter-logo"></a></li>
+        <li><a href="https://plus.google.com/share?url=http%3A%2F%2Fwww.kbsoftwaresolutions.com" target="_blank" 
+               title="Share on Google+" onclick="window.open('https://plus.google.com/share?url=' + encodeURIComponent(document.URL)); 
+          return false;"><img src="img/social-media/Google+.png" alt="google-logo"></a></li>
+        <li><a href="http://pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.kbsoftwaresolutions.com&description=" 
+               target="_blank" title="Pin it" onclick="window.open('http://pinterest.com/pin/create/button/?url=' + encodeURIComponent(document.URL) + '&description=' +  encodeURIComponent(document.title)); return false;">
+          <img src="img/social-media/Pinterest.png" alt="pinterest-logo"></a></li>
+        <li><a href="mailto:?subject=&body=:%20http%3A%2F%2Fwww.kbsoftwaresolutions.com" target="_blank" 
+               title="Email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) 
+          + '&body=' +  encodeURIComponent(document.URL)); return false;"><img src="img/social-media/Email.png" alt="email-logo"></a></li>
+      </ul>
+    </row>
 
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="col-lg-6">
+              <ul class="list-inline">
+                <li>
+                  <a href="index.php">Home</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li>
+                  <a href="view/contact.php">Contact</a>
+                </li>
+              </ul>
+              <p class="copyright text-muted small">Copyright &copy; C &amp; T Auto. All Rights Reserved</p>
+            </div>
+            <div class="col-lg-6">
+              <p class="copyright text-muted medium pull-right">
+                <img src="img/kb-logo-v3-75x60.png" alt="KB Software Solutions Logo">
+                <a href="http://www.kbsoftwaresolutions.com" target="_blank">Website Development by KB Software Solutions</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
-
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
-</body>
-
+  </body>
 </html>
